bd93df8926d963de25ab946240bd988d8adfaa91
Albert Galiullin <gah44@mail.ru>
2022-12-21 13:23:00 +0500

    Выполнено: added cod

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+
+if x1 != x2 and y1 != y2:
+
+  x_diff = x1 - x2
+  y_diff = y1 - y2
+  k = y_diff / x_diff
+  b = y2 - k * x2
+  print("Уравнение прямой, проходящей через эти точки:")
+  print("y = ", k, " * x + ", b)
+elif x1 == x2:
+  print("Уравнение прямой, проходящей через эти точки:")
+  print(" x =", x1)
+else:
+  print("Уравнение прямой, проходящей через эти точки:")
+  print("y =",y1)
