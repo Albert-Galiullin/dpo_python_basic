603f6ca1f72295a06af2c338767c10c72ebb393d
Albert Galiullin <gah44@mail.ru>
2022-12-21 13:58:07 +0500

    Выполнено

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+def summ(n):
+    print()
+    j = 0
+    while n != 0:
+        i = n % 10
+        j += i
+        n //= 10
+    print('Сумма цифр равна:', j)
+    return j
+    
+def quan(n):
+    print()
+    j = 0
+    while n != 0:
+        j += 1
+        n //= 10
+    print('Количество цифр равно:', j)
+    return j
+
+print()
+n = int(input('Введите число: '))
+a = summ(n)
+b = quan(n)
+print()
+print('Разность суммы и количества цифр:', a -b)
