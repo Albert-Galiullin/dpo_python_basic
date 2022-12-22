1c1a8b5a8f040410f773947e2083a06e1569f8c3
Albert Galiullin <gah44@mail.ru>
2022-12-22 15:40:28 +0500

    Выполнено

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+print()
+a = int(input('Введите первый год: '))
+b = int(input('Введите второй год: '))
+
+for i in range(a, b):
+    year = i
+    dig1 =  i // 1000
+    dig2 =  i // 100 - dig1 * 10
+
+    count_d1 = 0
+    for i in str(year):
+        if i == str(dig1):
+            count_d1 += 1
+
+    count_d2 = 0
+    for i in str(year):
+        if i == str(dig2):
+            count_d2 += 1
+
+    if count_d1 >= 3 or count_d2 >= 3:
+        print('Год: ', year)
