961e919e35cf07134a442f68ff9a5bd127f7bc8e
Albert Galiullin <gah44@mail.ru>
2022-12-22 14:55:46 +0500

    Выполнено

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def geo(x, y, r):
+  if x ** 2 + y ** 2 <= r ** 2:
+    print('Монетка где-то рядом')
+  else:
+    print('Монетки в области нет')
+
+
+print()
+x = float(input('Введите число x: '))
+y = float(input('Введите число y: '))
+r = float(input('Введите радиус: '))
+print()
+geo(x, y, r)
+
