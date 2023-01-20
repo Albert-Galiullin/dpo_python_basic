a1a4965ac27a3f0c8d71921dec69a8d7a2276f74
Albert Galiullin <gah44@mail.ru>
2023-01-20 18:11:45 +0500

    Задание 3 исправлено по замечаниям куратора

--- Module24/03_circle/main.py
+++ Module24/03_circle/main.py
@@ -1,36 +1,51 @@
 import math
-class Сircle:
+class Circle:
 
-    def __init__(self, x=0, y=0, r=1, k = 1):
+    def __init__(self, x=0, y=0, r=1):
         self.x = x
         self.y = y
         self.r = r
-        self.k = k
-    def check_info(self):
-        print('Площадь круга = ', math.pi * self.r ** 2 * self.k)
-        print('Периметр круга = ', math.pi * self.r * 2 * self.k)
+
+    def __str__(self):
+        return 'х = ' +str(self.x) + ' у = ' + str(self.y) + ' r = '+ str(self.r)
+
+    def get_area(self):
+        print('Площадь круга = ', round(self.r * self.r * math.pi,4))
+
+    def get_perimeter(self):
+        print('Периметр круга = ', round(2 * self.r * math.pi,4))
+
+    def scale(self, k):
+        self.r *= k
+        print(f'Радиус после увеличения в {k} раз = ', self.r)
+        return
+
+    def is_intersect(self, other):
+        if (self.x - other.x) ** 2 + (self.y - other.y) ** 2 <= (self.r + other.r) ** 2:
+            print('Окружности пересекаются')
+        else:
+            print('Окружности не пересекаются')
 
 
-print('Введите координаты первого круга')
-x1 = int(input('X = '))
-y1 = int(input('Y = '))
-r1 = int(input('Введите радиус круга: '))
-# k1 = int(input('Введите коэффициент увеличения круга: '))
 
-a = Сircle(x1, y1, r1)
-a.check_info()
+print('Окружность 1')
+c1 = Circle(0, 0, 1)
+print(c1)
+c1.get_area()
+c1.get_perimeter()
+c1.scale(2)
+c1.get_area()
+c1.get_perimeter()
 
-print('\nВведите координаты второго круга')
-x2 = int(input('X = '))
-y2 = int(input('Y = '))
-r2 = int(input('Введите радиус круга: '))
-# k2 = int(input('Введите коэффициент увеличения круга: '))
+print('\nОкружность 2')
 
-a = Сircle(x2, y2, r2)
-a.check_info()
+c2 = Circle(3, 3, 3)
+print(c2)
+c2.get_area()
+c2.get_perimeter()
+c2.scale(2)
+c2.get_area()
+c2.get_perimeter()
 
-c = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) # Определяем длину гипотенузы между центрами окружностей
-if r1 + r2 > c:
-    print('Окружности пересекаются')
-else:
-    print('Окружности не пересекаются')
+print('\nПересечение:')
+c1.is_intersect(c2)
