056f80c2522ccb88b4b2c3dbe6cd2a43667ab169
Albert Galiullin <gah44@mail.ru>
2023-01-21 23:08:15 +0500

    Задание 7 исправлено по замечаниям куратора

--- Module24/07_cohabitation/main.py
+++ Module24/07_cohabitation/main.py
@@ -6,32 +6,35 @@ class House:
 
 class Person:
 
-    def __init__(self, name):
+    def __init__(self, name, apartment):
         self.name = name
         self.satiety = 50
+        self.apartment = apartment
 
     def eat(self):  # ест
         self.satiety += 1
-        House.food -= 1
-        return f'ест, сытость {self.satiety} еда {House.food}'
+        apartment.food -= 1
+        return f'ест, сытость {self.satiety} еда {apartment.food}'
 
     def work(self):  # работает
         self.satiety -= 1
-        House.money += 1
-        return f'работает, сытость {self.satiety} деньги {House.money}'
+        apartment.money += 1
+        return f'работает, сытость {self.satiety} деньги {apartment.money}'
 
     def play(self):  # играет
         self.satiety -= 1
         return f'играет, сытость {self.satiety}'
 
     def repast(self):  # идет в магазин
-        House.food += 1
-        House.money -= 1
-        return f'идет в магазин, еда {House.food} деньги {House.money}'
+        apartment.food += 1
+        apartment.money -= 1
+        return f'идет в магазин, еда {apartment.food} деньги {apartment.money}'
 
 
-person_1 = Person('Вася')
-person_2 = Person('Федя')
+
+apartment = House()
+person_1 = Person('Вася', apartment)
+person_2 = Person('Федя',apartment)
 
 for i in range(365):  # пробуем прожить год
     number_cubes = randint(1, 6)
@@ -39,11 +42,11 @@ for i in range(365):  # пробуем прожить год
     if person.satiety < 0:  # голоден - умер. завершаем эксперимент
         print(f'К сожалению, {person.name} помер с голоду ')
         break
-    if person.satiety < 20 and House.food >= 10:
+    if person.satiety < 20 and apartment.food >= 10:
         text = person.eat()
-    elif House.food < 10:
+    elif apartment.food < 10:
         text = person.repast()
-    elif House.money < 50:
+    elif apartment.money < 50:
         text = person.work()
     elif number_cubes == 1:
         text = person.work()
