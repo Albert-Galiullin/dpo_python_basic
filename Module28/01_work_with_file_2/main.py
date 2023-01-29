7306a13ac6e6ade3563cde48429b70eef651cd5f
Albert Galiullin <gah44@mail.ru>
2023-01-29 20:16:15 +0500

    Выполнено

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+class File:
+
+    def __init__(self, filename: str, mode: str) -> None:
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        try:
+            self.file = open(self.filename, self.mode, encoding='utf-8')
+        except FileNotFoundError:
+            print('Файл не найден, создаем новый')
+            self.file = open(self.filename, "w", encoding='utf-8')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+        if exc_type:
+           print(exc_type, ' - Файл открыт в режиме ReadOnly, запись не удалась')
+           return True
+
+
+
+with File("example.txt", "r") as file:
+    file.write("Всем привет!")
+
--- Module28/03_modeling/main.py
+++ Module28/03_modeling/main.py
@@ -1,1 +1,78 @@
-# TODO здесь писать код
+import math
+
+
+
+class Square:
+    """Класс Квадрат"""
+    def __init__(self, le: int) -> int:
+        self.figure_name = "Квадрат"
+        self.le = le
+
+    @property
+    def square(self) -> float:
+        return self.le ** 2
+
+    @property
+    def perimeter(self) -> float:
+        return 4 * self.le
+
+
+class Triangle:
+    """Класс Треугольник"""
+    def __init__(self, le: int, h: int) ->int:
+        self.figure_name = "Треугольник"
+        self.le = le
+        self.h = h
+
+    @property
+    def square(self) -> float:
+        return 0.5 * self.le * self.h
+
+    @property
+    def perimeter(self) -> float:
+        return 2 * math.sqrt(self.h ** 2 + (self.le / 2) ** 2) + self.le
+
+
+class Cube(Square):
+    """ Класс Куб"""
+    def __init__(self, le: int) ->int:
+        super().__init__(le)
+        self.figure_name = 'Куб'
+
+    @property
+    def square(self) -> float:
+        return 6 * super().square
+
+    @property
+    def perimeter(self) ->int:
+        return 3 * super().perimeter
+
+
+class Pyramid(Triangle, Square):
+    """ Класс Пирамида"""
+    def __init__(self, le: int, h: int) -> int:
+        super().__init__(le, h)
+        self.figure_name = 'Пирамида'
+
+    @property
+    def square(self) -> int:
+        side_square = 4 * super().square
+        base_square = self.le ** 2
+        return side_square + base_square
+
+    @property
+    def perimeter(self) -> int:
+        return 2 * super().perimeter + 2 * self.le
+
+
+m = Square(2)
+print(m.square)
+print(m.perimeter)
+
+p = Pyramid(6, 4)
+print(p.square)
+print(p.perimeter)
+
+p = Cube(5)
+print(p.square)
+print(p.perimeter)
