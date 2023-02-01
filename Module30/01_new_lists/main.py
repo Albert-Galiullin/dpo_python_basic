15041f9d17c70e6ce6e66991dc4f76379944c69d
Albert Galiullin <gah44@mail.ru>
2023-02-01 15:09:36 +0500

    Выполнено

--- Module30/01_new_lists/main.py
+++ Module30/01_new_lists/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+from functools import reduce
+from typing import List
+
+floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
+names: List[str] = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
+numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
+
+
+# Каждое число из списка floats возводится в третью степень
+# и округляется до трёх знаков после запятой.
+floats_result: List[float] = list(map(lambda x: round(x ** 3, 3), floats))
+print(floats_result)
+
+# Из списка names берутся только те имена, в которых есть минимум пять букв.
+names_result: List[str] = list(filter(lambda x: len(x) >= 5, names))
+print(names_result)
+
+# Из списка numbers берётся произведение всех чисел.
+def my_add(a: int, b: int) -> int:
+    result = a * b
+    return result
+
+numbers_result: List[int] = list(map(lambda x: reduce(my_add, numbers), numbers))
+print(numbers_result)
--- Module30/03_code_readability/main.py
+++ Module30/03_code_readability/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+# Обычный и понятный код
+
+i = 1
+n = 1000
+prime_number = []
+
+while i <= n:
+    i += 1
+    for prime in prime_number:
+        if i % prime == 0:
+            break
+    else:
+        prime_number.append(i)
+
+print(prime_number)
+print()
+
+# Вариант 2.
+
+print(list(filter(lambda n: n > 1 and not any(n % i == 0 for i in range(2,n)), range(1000))))
+
+
